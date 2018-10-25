4e043ead80662f358786c98ca2e7d0524dacaf76
0mg <millig@outlook.jp>
2018-10-25 21:50:15 +0900

    upd: v0.7.0

--- main.h
+++ main.h
@@ -1,6 +1,6 @@
 #include <windows.h>
 // common defs (used by *.cpp and *.rc)
-#define C_APPVER 0,6,0,0
+#define C_APPVER 0,7,0,0
 #define C_APPNAME TEXT("Awaker")
 // icon
 #define C_APPICON 0x0001
